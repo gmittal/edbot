ce71189914f75ea28f94fef5d7c6a91e29ad98fc
gmittal <gautam@mittal.net>
2016-07-04 17:23:41 -0700

    init basic bot

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ var login = require("facebook-chat-api");
 
 // Create simple echo bot
 login({email: process.env.FB_EMAIL, password: process.env.FB_PASSWORD}, function callback (err, api) {
-    if(err) return console.error(err);
+  if(err) return console.error(err);
 
-    api.listen(function callback(err, message) {
-        api.sendMessage(message.body, message.threadID);
-    });
+  api.listen(function callback(err, message) {
+      api.sendMessage(message.body, message.threadID);
+  });
 });
