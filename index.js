85933ff12c47b45bfc54d43132228a53fd62a148
gmittal <gautam@mittal.net>
2016-07-19 10:38:48 -0700

    add wolfram alpha support

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 var dotenv = require('dotenv');
 dotenv.load();
+
 var wolfram = require('wolfram-alpha').createClient(process.env.WOLFRAM_APP_ID);
 var login = require("facebook-chat-api");
+var http = require('http');
+var fs = require('fs');
+var Hashids = require('hashids');
+var id = new Hashids("salt", 11);
 
-
+function rand_n() {
+  return Math.floor(Math.random()*100000000000000000000000)+1;
+}
 
 // Create simple echo bot
 login({email: process.env.FB_EMAIL, password: process.env.FB_PASSWORD}, function callback (err, api) {
@@ -11,18 +18,22 @@ login({email: process.env.FB_EMAIL, password: process.env.FB_PASSWORD}, function
 
   api.listen(function callback(err, message) {
       // Process the input
-      var m = message.body;
+      var query = message.body;
+
+      wolfram.query(query, function (err, result) {
+        if (err) throw err;
+
+        var messages = [];
+        for (var ix = 0; ix < result.length; ix++) {
+          var uid = id.encode(rand_n());
+          var file = fs.createWriteStream(__dirname+"/"+uid+".gif");
+          api.sendMessage(result[ix].title, message.threadID);
+        }
 
 
-      bot.setNick(message.senderID);
-      // Very basic cleverbot example
-      // bot.create(function (err, session) {
-          bot.ask(m, function (e, r) {
-            // and send back bot output
-            api.sendMessage(r, message.threadID);
-          });
-      // });
 
+
+      });
 
   });
 });
